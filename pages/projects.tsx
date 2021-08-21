3a1e2381fd74a4096d2928a02b822f364b007fa3
josephden16 <tegajoseph2002@gmail.com>
2021-08-21 23:18:33 +0100

    list projects based on rank

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -13,14 +13,24 @@ const seo = {
 
 
 const Projects = ({ links, projects }) => {
-  console.log(projects);
+  const sortingFunc = (a: any, b: any) => {
+    if (a.rank > b.rank) {
+      return -1;
+    }
+    if (a.rank < b.rank) {
+      return 1;
+    }
+    return 0;
+  }
+
+  let sortedProjects = projects.sort(sortingFunc);
   return (
     <>
       <Layout className="ml-3 mr-3 lg:ml-24 lg:mr-24 xl:ml-36 xl:mr-36 lg:mt-6 mb-10" seo={seo}>
         <div className="mb-32">
           <h1 className="mt-14 mb-12 lg:mb-16 lg:mt-12 text-3xl lg:text-4xl font-bold">My Projects 👨‍💻</h1>
           <div className="mt-7 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16">
-            {projects && projects.map(project => <ProjectCard key={project.id} project={project} />)}
+            {sortedProjects && sortedProjects.map(project => <ProjectCard key={project.id} project={project} />)}
           </div>
         </div>
         <Footer twitterLink={links.twitter_link} githubLink={links.github_link} linkedinLink={links.linkedin_link} />
@@ -35,22 +45,23 @@ const fetchProjects = async () => {
     const { data } = await client.query({
       query: gql`
       query {
-        projects(sort: "rank") {
+        projects {
           id
           name
           link
-          description
           rank
+          description
           image {
             alternativeText
             url
           }
         }
-      }      
+      }
       `
     });
 
-    return data.reverse();
+    return data;
+
   } catch (error) {
     return { projects: null };
   }
